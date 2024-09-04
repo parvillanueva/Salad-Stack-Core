a70e021ced7bf676a687cb56f08ed1dee5a93b2a
King Villanueva <king.villanueva@ballys.com>
2024-09-04 08:14:24 +0800

    [feat] fix Twigextension

--- src/View.php
+++ src/View.php
@@ -3,6 +3,7 @@ namespace Salad\Core;
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Salad\Core\TwigExtension;
 
 class View
 {
@@ -10,10 +11,24 @@ class View
 
     public function __construct()
     {
+        define('BASE_URL', Application::$ROOT_DIR . '/public/');
+
         $loader = new FilesystemLoader(Application::$ROOT_DIR . '/src/Views');
         $this->twig = new Environment($loader, [
             'cache' => false,
         ]);
+        $this->twig ->addExtension(new TwigExtension());
+        $this->twig->addFunction(new \Twig\TwigFunction('asset', function ($path) {
+            return $this->getBaseUrl() ."/" . ltrim($path, '/');
+        }));
+    }
+
+    function getBaseUrl() {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $hostName = $_SERVER['HTTP_HOST'];
+        $scriptPath = dirname($_SERVER['SCRIPT_NAME']);
+        $baseUrl = $protocol . $hostName . $scriptPath;
+        return rtrim($baseUrl, '/');
     }
 
     public function render($view, $data = [])
